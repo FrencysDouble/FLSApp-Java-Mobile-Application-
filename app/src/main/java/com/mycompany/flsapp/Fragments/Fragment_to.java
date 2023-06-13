96766aa8a10c3df25e9475c4baaad2538979dbc4
Glebas <zgleb@bk.ru>
2023-06-13 19:36:19 +0300

    Realize swipe down fragment

--- app/src/main/java/com/mycompany/flsapp/Fragments/Fragment_to.java
+++ app/src/main/java/com/mycompany/flsapp/Fragments/Fragment_to.java
@@ -1,19 +1,39 @@
 package com.mycompany.flsapp.Fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.fragment.app.DialogFragment;
+
+import com.github.andreilisun.swipedismissdialog.SwipeDismissDialog;
 import com.mycompany.flsapp.R;
 
 public class Fragment_to extends DialogFragment {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_to, container, false);
-        new SwipeDismissDialog.Builder(this).setView(rootView).build.show();
-        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialog);
-        return rootView;
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View rootView = inflater.inflate(R.layout.fragment_to, null);
+        SwipeDismissDialog swipeDismissDialog = new SwipeDismissDialog.Builder(requireContext())
+                .setView(rootView)
+                .setHorizontalOscillation(0f)
+                .build();
+
+
+        Dialog dialog = new Dialog(requireContext()) {
+            @Override
+            public void show() {
+                swipeDismissDialog.show();
+            }
+
+            @Override
+            public void dismiss() {
+                swipeDismissDialog.dismiss();
+            }
+        };
+        return dialog;
     }
+
 }
--- app/src/main/java/com/mycompany/flsapp/MainMenuActivity.java
+++ app/src/main/java/com/mycompany/flsapp/MainMenuActivity.java
@@ -1,22 +1,15 @@
 package com.mycompany.flsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.mycompany.flsapp.Fragments.Fragment_to;
 
 public class MainMenuActivity extends AppCompatActivity {
     EditText ed_to;
-    Fragment_to fragment_to = new Fragment_to();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +23,7 @@ public class MainMenuActivity extends AppCompatActivity {
     }
 
     public void openSearch1(View view) {
+        Fragment_to fragment_to = new Fragment_to();
         fragment_to.show(getSupportFragmentManager(), "dialogFragment");
     }
 }
